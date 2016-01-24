bb02a171ab33252880cd348636a59a5c3e9424a3
darkomega9408 <darkomega9408@yahoo.com>
2016-01-24 18:45:36 +0700

    + Added restful-java project

--- src/main/java/vn/khmt/restful/HelloService.java
+++ src/main/java/vn/khmt/restful/HelloService.java
@@ -1,16 +1,13 @@
 package vn.khmt.restful;
 
-import java.net.URISyntaxException;
-import java.sql.SQLException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 /**
@@ -19,50 +16,40 @@ import javax.ws.rs.core.Response;
  * @author TheNhan
  */
 
-@Path("hello")
+@Path("user")
 public class HelloService {
+    
+    String host = "ec2-54-227-253-228.compute-1.amazonaws.com";
+    String user = "uzufecmqojhnyx";
+    String port = "5432";
+    String pass = "WPJGueUbd3npLKslU2BEUOmMHx";
+    String dbname = "d8viikojj42e3b";
     
     @GET
     @Path("/{param}")
-    public Response getMsg(@PathParam("param") String msg) {
+    public Response getUser(@PathParam("param") int id) {
 
-        String output = "Hello " + msg;
+        ConnectToSQL db = new ConnectToSQL(ConnectToSQL.POSTGRESQL, host, dbname, user, pass);
+        Object[] user = db.getUser(id);
+        String res = "Username: " + user[1] + " - Email: " + user[2] + " - Name: " + user[3];
 
-        return Response.status(200).entity(output).build();
-    }
-    
-    @GET
-    @Path("/haha")
-    public Response retrieve() throws URISyntaxException, SQLException, ClassNotFoundException{
-        String host = "ec2-54-227-253-228.compute-1.amazonaws.com";
-        String user = "uzufecmqojhnyx";
-        String port = "5432";
-        String password = "WPJGueUbd3npLKslU2BEUOmMHx";
-        String dbname = "d8viikojj42e3b";
-        ConnectToSQL db = new ConnectToSQL(ConnectToSQL.POSTGRESQL, host, dbname, user, password);
-        
-        return Response.status(200).entity(db.checkUser("dino", "saurus")).build();
+        return Response.status(200).entity(res).build();
     }
 
-	@POST
-	@Path("/post")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response createTrackInJSON(@FormParam("username") String username,
-                @FormParam("password") String password,
-                @FormParam("email") String email,
-                @FormParam("status") int status,
-                @FormParam("name") String name) {
-            
-                    String host = "ec2-54-227-253-228.compute-1.amazonaws.com";
-        String user = "uzufecmqojhnyx";
-        String port = "5432";
-        String pass = "WPJGueUbd3npLKslU2BEUOmMHx";
-        String dbname = "d8viikojj42e3b";
+    @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createTrackInJSON(@FormParam("username") String username,
+            @FormParam("password") String password,
+            @FormParam("email") String email,
+            @FormParam("status") int status,
+            @FormParam("name") String name) {
+
         ConnectToSQL db = new ConnectToSQL(ConnectToSQL.POSTGRESQL, host, dbname, user, pass);
         
         
 
-		return Response.status(201).entity(db.addUser(username, password, email, status, name)).build();
-		
-	}
+        return Response.status(201).entity(db.addUser(username, password, email, status, name)).build();
+
+    }
 }
